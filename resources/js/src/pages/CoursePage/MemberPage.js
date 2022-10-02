4ae3dabd1d1fb28c9c321a67e58ffe93d84fff0e
richardktran.dev <richardktran.dev@gmail.com>
2022-10-02 22:33:55 +0700

    fix: sidebar redirect

--- resources/js/src/pages/CoursePage/MemberPage.js
+++ resources/js/src/pages/CoursePage/MemberPage.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { userApi } from '../../api/userApi';
+import { changePage } from '../../app/reducers/sideBarReducer';
 import Layout from '../../components/Layout/Layout';
 import AddMemberModal from '../../components/Modal/AddMemberModal';
 import TeamList from '../../components/TeamList/TeamList';
 import { HTTP_OK } from '../../utils/constant';
+import { courseDetailItems } from './sidebars/courseDetail';
 
 const MemberPage = () => {
   let { id } = useParams(); //get id from url
+
+  const dispatch = useDispatch();
+  const sidebarItems = useSelector(state => state.sidebar);
+
+  useEffect(() => {
+    const action = changePage(sidebarItems.length === 0 ? courseDetailItems : sidebarItems);
+    dispatch(action);
+  }, []);
 
   const [showMemberModal, setShowMemberModal] = useState(false);
 
@@ -30,62 +41,60 @@ const MemberPage = () => {
 
 
   return (
-    <Layout>
-      <div className="container-fluid">
-        <div className="nk-content-inner">
-          <div className="nk-content-body">
-            <div className="nk-block-head nk-block-head-sm">
-              <div className="nk-block-between">
-                <div className="nk-block-head-content">
-                  <div className="nk-block-head-sub mb-2">
-                    <Link className="back-to" to={`/courses/${id}`} >
-                      <em className="icon ni ni-arrow-left" />
-                      <span>Trở lại</span>
-                    </Link>
-                  </div>
-                  <h3 className="nk-block-title page-title">Thành viên - Luận văn tốt nghiệp</h3>
-                </div>{/* .nk-block-head-content */}
-                <div className="nk-block-head-content">
-                  <div className="nk-block-head-sub mb-2"></div>
-                  <div className="toggle-wrap nk-block-tools-toggle">
-                    <a href="#" className="btn btn-icon btn-trigger toggle-expand mr-n1" data-target="pageMenu"><em className="icon ni ni-more-v" /></a>
-                    <div className="toggle-expand-content" data-content="pageMenu">
-                      <ul className="nk-block-tools g-3">
-                        <li>
-                          <div className="drodown">
-                            <a href="#" className="dropdown-toggle btn btn-white btn-dim btn-outline-light" data-toggle="dropdown"><em className="d-none d-sm-inline icon ni ni-calender-date" /><span><span className="d-none d-md-inline">Last</span> 30 Days</span><em className="dd-indc icon ni ni-chevron-right" /></a>
-                            <div className="dropdown-menu dropdown-menu-right">
-                              <ul className="link-list-opt no-bdr">
-                                <li><a href="#"><span>Last 30 Days</span></a></li>
-                              </ul>
-                            </div>
+    <div className="container-fluid">
+      <div className="nk-content-inner">
+        <div className="nk-content-body">
+          <div className="nk-block-head nk-block-head-sm">
+            <div className="nk-block-between">
+              <div className="nk-block-head-content">
+                <div className="nk-block-head-sub mb-2">
+                  <Link className="back-to" to={"/courses"} >
+                    <em className="icon ni ni-arrow-left" />
+                    <span>Trở lại</span>
+                  </Link>
+                </div>
+                <h3 className="nk-block-title page-title">Thành viên - Luận văn tốt nghiệp</h3>
+              </div>{/* .nk-block-head-content */}
+              <div className="nk-block-head-content">
+                <div className="nk-block-head-sub mb-2"></div>
+                <div className="toggle-wrap nk-block-tools-toggle">
+                  <a href="#" className="btn btn-icon btn-trigger toggle-expand mr-n1" data-target="pageMenu"><em className="icon ni ni-more-v" /></a>
+                  <div className="toggle-expand-content" data-content="pageMenu">
+                    <ul className="nk-block-tools g-3">
+                      <li>
+                        <div className="drodown">
+                          <a href="#" className="dropdown-toggle btn btn-white btn-dim btn-outline-light" data-toggle="dropdown"><em className="d-none d-sm-inline icon ni ni-calender-date" /><span><span className="d-none d-md-inline">Last</span> 30 Days</span><em className="dd-indc icon ni ni-chevron-right" /></a>
+                          <div className="dropdown-menu dropdown-menu-right">
+                            <ul className="link-list-opt no-bdr">
+                              <li><a href="#"><span>Last 30 Days</span></a></li>
+                            </ul>
                           </div>
-                        </li>
-                        <li className="nk-block-tools-opt">
-                          <a href="#" onClick={() => setShowMemberModal(true)} className="btn btn-primary">
-                            <em className="icon ni ni-reports" />
-                            <span>Thêm thành viên</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
+                        </div>
+                      </li>
+                      <li className="nk-block-tools-opt">
+                        <a href="#" onClick={() => setShowMemberModal(true)} className="btn btn-primary">
+                          <em className="icon ni ni-reports" />
+                          <span>Thêm thành viên</span>
+                        </a>
+                      </li>
+                    </ul>
                   </div>
-                </div>{/* .nk-block-head-content */}
-              </div>{/* .nk-block-between */}
-            </div>
-            <div className="nk-block">
-              <TeamList />
-            </div>
-            <AddMemberModal
-              modalName="Thêm thành viên"
-              onFinish={addMember}
-              isShow={showMemberModal}
-              handleCloseModal={() => setShowMemberModal(false)}
-            />
+                </div>
+              </div>{/* .nk-block-head-content */}
+            </div>{/* .nk-block-between */}
           </div>
+          <div className="nk-block">
+            <TeamList />
+          </div>
+          <AddMemberModal
+            modalName="Thêm thành viên"
+            onFinish={addMember}
+            isShow={showMemberModal}
+            handleCloseModal={() => setShowMemberModal(false)}
+          />
         </div>
       </div>
-    </Layout>
+    </div>
   );
 }
 
--- resources/js/src/pages/CoursePage/index.js
+++ resources/js/src/pages/CoursePage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-loading';
+import Layout from '../../components/Layout/Layout';
 import HomePage from '../HomePage/HomePage';
 import CourseDetailPage from './CourseDetailPage';
 import MemberPage from './MemberPage';
@@ -7,11 +8,13 @@ import MemberPage from './MemberPage';
 function Course() {
 
   return (
-    <Routes>
-      <Route path="" element={<HomePage />} />
-      <Route path=":id/info" element={<CourseDetailPage />} />
-      <Route path=":id/members" element={<MemberPage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path=":id/info" element={<CourseDetailPage />} />
+        <Route path=":id/members" element={<MemberPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
