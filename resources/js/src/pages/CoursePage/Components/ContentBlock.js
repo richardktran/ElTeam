e0fa257888d3b41d36756be82328cff6e808a3b3
richardktran.dev <richardktran.dev@gmail.com>
2022-10-12 00:12:06 +0700

    feat: custom divider for curriculum page

--- resources/js/src/pages/CoursePage/Components/ContentBlock.js
+++ resources/js/src/pages/CoursePage/Components/ContentBlock.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 function ContentBlock(props) {
   const { pageId, html, tag } = props;
-  const CustomTag = tag;
-  return (
-    <CustomTag>{html}</CustomTag>
-  )
+  const CustomTag = (html) => {
+    const Tag = tag;
+    switch (tag) {
+      case 'divider':
+        return <hr style={{ border: '1px solid rgba(55, 53, 47, 0.16)' }} />;
+      default:
+        return <Tag>{html}</Tag>;
+    }
+  };
+  return CustomTag(html);
 }
 
 export default ContentBlock
