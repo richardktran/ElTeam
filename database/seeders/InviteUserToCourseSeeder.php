3ecbdc0389b8ac3f0e0b0ac9ec3c4bf0d6f103ff
richardktran <richardktran.dev@gmail.com>
2022-10-23 22:25:47 +0700

    create seeder to invite all user to a course

--- database/seeders/InviteUserToCourseSeeder.php
+++ database/seeders/InviteUserToCourseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
+use App\Models\CourseStudent;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class InviteUserToCourseSeeder extends Seeder
@@ -13,6 +16,23 @@ class InviteUserToCourseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $course = Course::find(1);
+        $students = User::whereNotIn('id', [$course->teacher_id])->get();
+        foreach ($students as $student) {
+            $course->students()->attach($student->id, [
+                'status' => $this->getRandomStatus()
+            ]);
+        }
+    }
+
+    private function getRandomStatus()
+    {
+        $statuses = [
+            CourseStudent::STATUS_PENDING,
+            CourseStudent::STATUS_ACCEPTED,
+            CourseStudent::STATUS_DECLINED
+        ];
+
+        return $statuses[array_rand($statuses)];
     }
 }
