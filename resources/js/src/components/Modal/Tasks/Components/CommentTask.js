458028c03cc9bcebe48bbc490bc46ded7d55a3f3
richardktran <richardktran.dev@gmail.com>
2022-11-03 00:12:39 +0700

    move comment form up

--- resources/js/src/components/Modal/Tasks/Components/CommentTask.js
+++ resources/js/src/components/Modal/Tasks/Components/CommentTask.js
@@ -10,6 +10,163 @@ function CommentTask() {
         <div className="simplebar-mask">
           <div className="simplebar-offset" style={{ right: '0px', bottom: '0px' }}>
             <div className="simplebar-content-wrapper" style={{ height: '100%', overflow: 'hidden scroll' }}>
+              {/* .nk-reply-item */}
+              <div className="nk-reply-form" style={{ marginBottom: "0" }}>
+                <div className="nk-reply-form-header">
+                  <ul className="nav nav-tabs-s2 nav-tabs nav-tabs-sm">
+                    <li className="nav-item">
+                      <a className="nav-link active" data-toggle="tab" href="#reply-form">Reply</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" data-toggle="tab" href="#note-form">Private Note</a>
+                    </li>
+                  </ul>
+                  <div className="nk-reply-form-title">
+                    <div className="title">Reply as:</div>
+                    <div className="user-avatar xs bg-purple">
+                      <span>IH</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="tab-content">
+                  <div className="tab-pane active" id="reply-form">
+                    <div className="nk-reply-form-editor">
+                      <div className="nk-reply-form-field">
+                        <textarea className="form-control form-control-simple no-resize" placeholder="Hello" defaultValue={""} />
+                      </div>
+                      <div className="nk-reply-form-tools">
+                        <ul className="nk-reply-form-actions g-1">
+                          <li className="mr-2">
+                            <button className="btn btn-primary" type="submit">Reply</button>
+                          </li>
+                          <li>
+                            <div className="dropdown">
+                              <a className="btn btn-icon btn-sm btn-tooltip" data-toggle="dropdown" title href="#" data-original-title="Templates">
+                                <em className="icon ni ni-hash" />
+                              </a>
+                              <div className="dropdown-menu">
+                                <ul className="link-list-opt no-bdr link-list-template">
+                                  <li className="opt-head">
+                                    <span>Quick Insert</span>
+                                  </li>
+                                  <li>
+                                    <a href="#">
+                                      <span>Thank you message</span>
+                                    </a>
+                                  </li>
+                                  <li>
+                                    <a href="#">
+                                      <span>Your issues solved</span>
+                                    </a>
+                                  </li>
+                                  <li>
+                                    <a href="#">
+                                      <span>Thank you message</span>
+                                    </a>
+                                  </li>
+                                  <li className="divider"></li>
+                                  <li>
+                                    <a href="#">
+                                      <em className="icon ni ni-file-plus" />
+                                      <span>Save as Template</span>
+                                    </a>
+                                  </li>
+                                  <li>
+                                    <a href="#">
+                                      <em className="icon ni ni-notes-alt" />
+                                      <span>Manage Template</span>
+                                    </a>
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Upload Attachment">
+                              <em className="icon ni ni-clip-v" />
+                            </a>
+                          </li>
+                          <li>
+                            <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Insert Emoji">
+                              <em className="icon ni ni-happy" />
+                            </a>
+                          </li>
+                          <li>
+                            <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Upload Images">
+                              <em className="icon ni ni-img" />
+                            </a>
+                          </li>
+                        </ul>
+                        <div className="dropdown">
+                          <a href="#" className="dropdown-toggle btn-trigger btn btn-icon mr-n2" data-toggle="dropdown">
+                            <em className="icon ni ni-more-v" />
+                          </a>
+                          <div className="dropdown-menu dropdown-menu-right">
+                            <ul className="link-list-opt no-bdr">
+                              <li>
+                                <a href="#">
+                                  <span>Another Option</span>
+                                </a>
+                              </li>
+                              <li>
+                                <a href="#">
+                                  <span>More Option</span>
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      {/* .nk-reply-form-tools */}
+
+                    </div>
+                    {/* .nk-reply-form-editor */}
+
+                  </div>
+                  <div className="tab-pane" id="note-form">
+                    <div className="nk-reply-form-editor">
+                      <div className="nk-reply-form-field">
+                        <textarea className="form-control form-control-simple no-resize" placeholder="Type your private note, that only visible to internal team." defaultValue={""} />
+                      </div>
+                      <div className="nk-reply-form-tools">
+                        <ul className="nk-reply-form-actions g-1">
+                          <li className="mr-2">
+                            <button className="btn btn-primary" type="submit">Add Note</button>
+                          </li>
+                          <li>
+                            <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Upload Attachment">
+                              <em className="icon ni ni-clip-v" />
+                            </a>
+                          </li>
+                        </ul>
+                        <div className="dropdown">
+                          <a href="#" className="dropdown-toggle btn-trigger btn btn-icon mr-n2" data-toggle="dropdown">
+                            <em className="icon ni ni-more-v" />
+                          </a>
+                          <div className="dropdown-menu dropdown-menu-right">
+                            <ul className="link-list-opt no-bdr">
+                              <li>
+                                <a href="#">
+                                  <span>Another Option</span>
+                                </a>
+                              </li>
+                              <li>
+                                <a href="#">
+                                  <span>More Option</span>
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      {/* .nk-reply-form-tools */}
+
+                    </div>
+                    {/* .nk-reply-form-editor */}
+                  </div>
+                </div>
+              </div>
+              {/* .nk-reply-form */}
               <div className="simplebar-content" style={{ padding: '0px' }}>
                 <div className="nk-reply-item">
                   <div className="nk-reply-header">
@@ -124,164 +281,7 @@ function CommentTask() {
                     <div className="nk-reply-from"> Replied by <span>Noor Parvez</span> at 11:32 AM </div>
                   </div>
                 </div>
-                {/* .nk-reply-item */}
-                <div className="nk-reply-form">
-                  <div className="nk-reply-form-header">
-                    <ul className="nav nav-tabs-s2 nav-tabs nav-tabs-sm">
-                      <li className="nav-item">
-                        <a className="nav-link active" data-toggle="tab" href="#reply-form">Reply</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" data-toggle="tab" href="#note-form">Private Note</a>
-                      </li>
-                    </ul>
-                    <div className="nk-reply-form-title">
-                      <div className="title">Reply as:</div>
-                      <div className="user-avatar xs bg-purple">
-                        <span>IH</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="tab-content">
-                    <div className="tab-pane active" id="reply-form">
-                      <div className="nk-reply-form-editor">
-                        <div className="nk-reply-form-field">
-                          <textarea className="form-control form-control-simple no-resize" placeholder="Hello" defaultValue={""} />
-                        </div>
-                        <div className="nk-reply-form-tools">
-                          <ul className="nk-reply-form-actions g-1">
-                            <li className="mr-2">
-                              <button className="btn btn-primary" type="submit">Reply</button>
-                            </li>
-                            <li>
-                              <div className="dropdown">
-                                <a className="btn btn-icon btn-sm btn-tooltip" data-toggle="dropdown" title href="#" data-original-title="Templates">
-                                  <em className="icon ni ni-hash" />
-                                </a>
-                                <div className="dropdown-menu">
-                                  <ul className="link-list-opt no-bdr link-list-template">
-                                    <li className="opt-head">
-                                      <span>Quick Insert</span>
-                                    </li>
-                                    <li>
-                                      <a href="#">
-                                        <span>Thank you message</span>
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a href="#">
-                                        <span>Your issues solved</span>
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a href="#">
-                                        <span>Thank you message</span>
-                                      </a>
-                                    </li>
-                                    <li className="divider"></li>
-                                    <li>
-                                      <a href="#">
-                                        <em className="icon ni ni-file-plus" />
-                                        <span>Save as Template</span>
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a href="#">
-                                        <em className="icon ni ni-notes-alt" />
-                                        <span>Manage Template</span>
-                                      </a>
-                                    </li>
-                                  </ul>
-                                </div>
-                              </div>
-                            </li>
-                            <li>
-                              <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Upload Attachment">
-                                <em className="icon ni ni-clip-v" />
-                              </a>
-                            </li>
-                            <li>
-                              <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Insert Emoji">
-                                <em className="icon ni ni-happy" />
-                              </a>
-                            </li>
-                            <li>
-                              <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Upload Images">
-                                <em className="icon ni ni-img" />
-                              </a>
-                            </li>
-                          </ul>
-                          <div className="dropdown">
-                            <a href="#" className="dropdown-toggle btn-trigger btn btn-icon mr-n2" data-toggle="dropdown">
-                              <em className="icon ni ni-more-v" />
-                            </a>
-                            <div className="dropdown-menu dropdown-menu-right">
-                              <ul className="link-list-opt no-bdr">
-                                <li>
-                                  <a href="#">
-                                    <span>Another Option</span>
-                                  </a>
-                                </li>
-                                <li>
-                                  <a href="#">
-                                    <span>More Option</span>
-                                  </a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                        {/* .nk-reply-form-tools */}
 
-                      </div>
-                      {/* .nk-reply-form-editor */}
-
-                    </div>
-                    <div className="tab-pane" id="note-form">
-                      <div className="nk-reply-form-editor">
-                        <div className="nk-reply-form-field">
-                          <textarea className="form-control form-control-simple no-resize" placeholder="Type your private note, that only visible to internal team." defaultValue={""} />
-                        </div>
-                        <div className="nk-reply-form-tools">
-                          <ul className="nk-reply-form-actions g-1">
-                            <li className="mr-2">
-                              <button className="btn btn-primary" type="submit">Add Note</button>
-                            </li>
-                            <li>
-                              <a className="btn btn-icon btn-sm" data-toggle="tooltip" data-placement="top" title href="#" data-original-title="Upload Attachment">
-                                <em className="icon ni ni-clip-v" />
-                              </a>
-                            </li>
-                          </ul>
-                          <div className="dropdown">
-                            <a href="#" className="dropdown-toggle btn-trigger btn btn-icon mr-n2" data-toggle="dropdown">
-                              <em className="icon ni ni-more-v" />
-                            </a>
-                            <div className="dropdown-menu dropdown-menu-right">
-                              <ul className="link-list-opt no-bdr">
-                                <li>
-                                  <a href="#">
-                                    <span>Another Option</span>
-                                  </a>
-                                </li>
-                                <li>
-                                  <a href="#">
-                                    <span>More Option</span>
-                                  </a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                        {/* .nk-reply-form-tools */}
-
-                      </div>
-                      {/* .nk-reply-form-editor */}
-
-                    </div>
-                  </div>
-                </div>
-                {/* .nk-reply-form */}
 
               </div>
             </div>
