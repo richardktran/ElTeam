f9b4000d1ce90b9dfaa78c91a43a27e59ced915a
richardktran <richardktran.dev@gmail.com>
2022-11-15 22:40:08 +0700

    update UI for email list

--- resources/js/src/components/Modal/AddMemberModal.js
+++ resources/js/src/components/Modal/AddMemberModal.js
@@ -5,17 +5,23 @@ import BaseModal from './BaseModal'
 import toast from 'react-hot-toast';
 import Dragger from 'antd/lib/upload/Dragger';
 import { InboxOutlined } from '@mui/icons-material';
+import { useEffect } from 'react';
 
 const AddMemberModal = (props) => {
     const { isShow, modalName, onFinish, handleCloseModal, courseId } = props
     const [type, setType] = useState('multiple');
     const [modalSize, setModalSize] = useState('xs');
+    const [checkAll, setCheckAll] = useState(true);
+
     const [emailList, setEmailList] = useState(null);
 
-    const emailListExample = [
-        'aaa@gg.com',
-        'bbb@gg.com'
-    ];
+    useEffect(() => {
+        if (emailList === null) {
+            return;
+        }
+        const isCheckAll = emailList.every((item) => item.checked);
+        setCheckAll(isCheckAll);
+    }, [emailList]);
 
     const uploadProps = {
         name: 'file',
@@ -27,7 +33,15 @@ const AddMemberModal = (props) => {
                 console.log(info.file, info.fileList);
             }
             if (status === 'done') {
-                setEmailList(response.data);
+                const emails = response.data;
+                // Add checked field to each email with true value default
+                const emailsChecked = emails.map((email) => {
+                    return {
+                        email: email,
+                        checked: true
+                    }
+                });
+                setEmailList(emailsChecked);
             } else if (status === 'error') {
                 toast.error(`${info.file.name} file upload failed.`);
             }
@@ -38,13 +52,23 @@ const AddMemberModal = (props) => {
     };
 
     const handleSelectChange = (value) => {
-        if (value.value === 'multiple') {
-            setModalSize('lg');
-        } else {
-            setModalSize('xs');
-        }
         setType(value.value);
     }
+
+    const handleCheckAll = () => {
+        setCheckAll(!checkAll);
+        setEmailList(emailList.map((item) => {
+            return {
+                ...item,
+                checked: !checkAll,
+            }
+        }));
+    }
+
+    const uploadAgain = () => {
+        setEmailList(null);
+    }
+
     return (
         <BaseModal modalName={modalName} handleCloseModal={handleCloseModal} isShow={isShow} modalSize={modalSize}>
             <Form action="#" className="form-validate is-alter" onFinish={onFinish}>
@@ -107,20 +131,63 @@ const AddMemberModal = (props) => {
                         </p>
                     </Dragger>
                 }
-                <ul class="custom-control-group custom-control-vertical w-100 mb-3">
-                    {emailListExample.map((email, index) => (
-                        <li>
-                            <div class="custom-control custom-control-sm custom-checkbox custom-control-pro">
-                                <input type="checkbox" class="custom-control-input" id="user-choose-s1" name="user-choose" />
-                                <label class="custom-control-label" for="user-choose-s1">
-                                    <span class="user-card">
-                                        <span class="user-name">Keith Jensen</span>
-                                    </span>
-                                </label>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+
+                {type === 'multiple' && emailList !== null &&
+                    <div className="email_list">
+                        <h6 className="title my-3">
+                            Danh sách email thành viên
+                            <span className="ml-2">
+                                <a onClick={uploadAgain} className="btn btn-dim btn-sm btn-outline-light">
+                                    <em class="icon ni ni-plus-circle-fill"></em>
+                                    <span>Nhập file khác</span>
+                                </a>
+                            </span>
+                        </h6>
+                        {/* Create a div with max height and accept scroll */}
+
+                        <div class="custom-control custom-control-sm custom-checkbox custom-control-pro">
+                            <input
+                                type="checkbox"
+                                class="custom-control-input"
+                                id={`check-all`}
+                                checked={checkAll}
+                                onChange={handleCheckAll}
+                            />
+                            <label class="custom-control-label" style={{ borderWidth: '0' }} for={`check-all`}>
+                                <span class="user-card">
+                                    <span class="user-name">Chọn tất cả</span>
+                                </span>
+                            </label>
+                        </div>
+                        <div className="scrollable mb-3" style={{ maxHeight: '300px', overflowY: 'scroll', border: '1px solid #dbdfea' }}>
+                            <ul class="custom-control-group custom-control-vertical w-100 mb-3">
+                                {emailList && emailList.map((email, index) => (
+                                    <li>
+                                        <div class="custom-control custom-control-sm custom-checkbox custom-control-pro">
+                                            <input
+                                                type="checkbox"
+                                                class="custom-control-input"
+                                                id={`user-choose-s${index}`}
+                                                checked={email.checked}
+                                                onChange={(e) => {
+                                                    const newEmailList = [...emailList];
+                                                    newEmailList[index].checked = e.target.checked;
+                                                    setEmailList(newEmailList);
+                                                    console.log(newEmailList);
+                                                }}
+                                            />
+                                            <label class="custom-control-label" style={{ borderWidth: '0' }} for={`user-choose-s${index}`}>
+                                                <span class="user-card">
+                                                    <span class="user-name">{email.email}</span>
+                                                </span>
+                                            </label>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                }
 
                 <Form.Item className="form-group">
                     <button type="submit" className="btn btn-lg btn-primary">Thêm học viên</button>
