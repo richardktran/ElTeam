7629711e7b645727bf65aa947b4614fda09f9ae4
richardktran <richardktran.dev@gmail.com>
2022-11-19 23:11:51 +0700

    add new logo

--- resources/js/src/components/Sidebar/HomeSidebar.js
+++ resources/js/src/components/Sidebar/HomeSidebar.js
@@ -36,10 +36,8 @@ function HomeSidebar({ pathname }) {
     <div className="nk-sidebar nk-sidebar-fixed is-light" data-content="sidebarMenu">
       <div className="nk-sidebar-element nk-sidebar-head">
         <div className="nk-sidebar-brand">
-          <a href="html/index.html" className="logo-link nk-sidebar-logo">
-            <img className="logo-light logo-img" src="./images/logo.png" srcSet="./images/logo2x.png 2x" alt="logo" />
-            <img className="logo-dark logo-img" src="./images/logo-dark.png" srcSet="./images/logo-dark2x.png 2x" alt="logo-dark" />
-            <img className="logo-small logo-img logo-img-small" src="./images/logo-small.png" srcSet="./images/logo-small2x.png 2x" alt="logo-small" />
+          <a href="/">
+            <img width={200} height={50} className="logo-img logo-img-lg" src="/images/elteam_logo.png" alt="logo-small" />
           </a>
         </div>
         <div className="nk-menu-trigger mr-n2">
