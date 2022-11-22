24e36e267a6583c1b6a7e6b3bc0da6d3243b8d5e
richardktran <richardktran.dev@gmail.com>
2022-11-22 22:53:52 +0700

    add content and footer for landing page

--- resources/js/src/pages/HomePage/LandingPage.js
+++ resources/js/src/pages/HomePage/LandingPage.js
@@ -8,18 +8,20 @@ function LandingPage() {
   return (
     <div id="wrapper" className="clearfix">
       <section id="slider" className="slider-element slider-parallax min-vh-100 min-vh-md-100 slider-parallax-visible">
-        <div className="slider-inner" style={{ background: 'url("https://elteam.s3.ap-southeast-1.amazonaws.com/images/appshowcase/1.jpg") 0% 0% / cover', height: '100vh' }}>
+        <div className="slider-inner" >
+          <div style={{ background: 'url("https://elteam.s3.ap-southeast-1.amazonaws.com/images/appshowcase/1.jpg") 0% 0% / cover', height: '100vh', width: '100vw', zIndex: '-1000', position: "fixed", top: 0, left: 0, filter: "blur(5px)", }}></div>
           <div className="container-fluid">
             <img src="/images/elteam_logo.png" width={600} height={200} className="d-none d-lg-block" style={{ position: 'absolute', left: 0, top: 20 }} />
             {/* <img src="https://elteam.s3.ap-southeast-1.amazonaws.com/images/appshowcase/slider-iphone.png" alt="Image" className="d-none d-lg-block" style={{ position: 'absolute', left: 20, bottom: 0 }} /> */}
-            <div className="vertical-middle">
+            <div className="vertical-middle" >
               <div className="row">
                 <div className="col-lg-5 offset-lg-7 col-xl-6 offset-xl-6 dark fadeIn animated" data-animate="fadeIn">
                   <div className="heading-block border-bottom-0">
-                    <h1 className="nott">The new way to create awesome websites. Try using <strong>Canvas</strong> Template. </h1>
+                    <h1 className="nott" style={{ fontSize: '50px', color: "rgb(104 255 207)" }}>Học tập theo nhóm một cách hiệu quả với ELTEAM</h1>
+                    <p className="nott mt-3" style={{ fontSize: '18px', color: "white" }}>Đăng ký ngay để tham gia lớp học của bạn và làm việc nhóm với mọi người một cách thật hiệu quả nhé 🚀</p>
                   </div>
-                  <a href="#" className="button button-light button-xlarge button-rounded m-0">
-                    <i className="icon-apple" />Start Free Trial </a>
+                  <a href="/login" className="button button-primary button-xlarge button-rounded m-0">
+                    <i className="icon-apple" />THAM GIA NGAY </a>
                 </div>
               </div>
             </div>
@@ -35,22 +37,22 @@ function LandingPage() {
                 <a href="#">
                   <img src="https://elteam.s3.ap-southeast-1.amazonaws.com/images/appshowcase/s1.png" alt="Image" className="bottommargin-sm" />
                 </a>
-                <h4>Responsive Layout</h4>
-                <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
+                <h4>THEO DÕI CÔNG VIỆC</h4>
+                <p>Quản lý công việc của các thành viên trong nhóm thông qua Kanban board.</p>
               </div>
               <div className="col-sm-6 col-lg-4 text-center">
                 <a href="#">
                   <img src="https://elteam.s3.ap-southeast-1.amazonaws.com/images/appshowcase/s4.png" alt="Image" className="bottommargin-sm" />
                 </a>
-                <h4>Retina Ready Graphics</h4>
-                <p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
+                <h4>THỜI GIAN THỰC</h4>
+                <p>Bình luận, hỗ trợ để giúp đỡ nhau thông qua bình luận và thông báo bằng thời gian thực.</p>
               </div>
               <div className="col-sm-6 col-lg-4 text-center">
                 <a href="#">
                   <img src="https://elteam.s3.ap-southeast-1.amazonaws.com/images/appshowcase/s5.png" alt="Image" className="bottommargin-sm" />
                 </a>
-                <h4>Powerful Performance</h4>
-                <p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
+                <h4>TÙY BIẾN NỘI DUNG ĐA DẠNG</h4>
+                <p>Có thể tự tạo nội dung bài giảng cho riêng mình với đa dạng các loại dữ liệu (video, hình ảnh, file, văn bản,...)</p>
               </div>
             </div>
           </div>
@@ -60,12 +62,12 @@ function LandingPage() {
               <div className="row">
                 <div className="col-lg-5">
                   <div className="heading-block topmargin-sm">
-                    <h2>Awesome Scalable Apps</h2>
-                    <span>Our Template acts &amp; behaves truly as a Canvas.</span>
+                    <h2>QUẢN LÝ CÔNG VIỆC NHÓM</h2>
+                    <span>Phương pháp quản lý công việc hiện đại và trực quan với Kanban board.</span>
                   </div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem maiores pariatur voluptatem placeat laborum iste accusamus nam unde, iure id.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet cumque, perferendis accusamus porro illo exercitationem molestias, inventore obcaecati ut omnis voluptatibus ratione.</p>
-                  <a href="#" className="button button-border button-rounded button-large button-dark ms-0">Start Trial</a>
+                  <p>Với Kanban board, bạn có thể quản lý được các tiến độ công việc của nhóm mình cũng như là đo được chất lượng làm việc của người chịu trách nhiệm cho công việc đó.</p>
+                  <p>Theo dõi được mức độ hiệu quả trong công việc của các thành viên trong nhóm, phối hợp giữa các thành viên trong nhóm một cách mượt mà hơn.</p>
+                  <a href="/login" className="button button-border button-rounded button-large button-dark ms-0">Thử ngay</a>
                 </div>
               </div>
             </div>
@@ -78,124 +80,46 @@ function LandingPage() {
                 </div>
                 <div className="col-md-6 text-center text-md-start">
                   <div className="heading-block border-bottom-0">
-                    <h2>Typically Responsive</h2>
-                    <span>Our App scales beautifully to different Devices.</span>
+                    <h2>Quản lý lớp học</h2>
+                    <span>Hệ thống giúp giáo viên có thể quản lý lớp học dễ dàng.</span>
                   </div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet cumque, perferendis accusamus porro illo exercitationem molestias, inventore obcaecati ut omnis voluptatibus ratione odio amet magnam quidem tempore necessitatibus quaerat, voluptates excepturi voluptatem, veritatis qui temporibus.</p>
-                  <a href="#" className="button button-border button-rounded button-large button-dark ms-0">View Gallery</a>
+                  <p>Với hệ thống quản lý tài nguyên học tập, thầy cô có thể tạo những bài giảng cũng như là soạn nội dung bài học trực tiếp trên hệ thống với đa dạng các thể loại tập tin như văn bản, hình ảnh, âm thanh,... </p>
+                  <a href="/login" className="button button-border button-rounded button-large button-dark ms-0">THỬ NGAY</a>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="section dark mt-0 pb-0" style={{ paddingTop: '60px' }}>
-            <div className="container clearfix">
-              <div className="row col-mb-50">
-                <div className="col-md-6 col-lg-4">
-                  <div className="feature-box fbox-plain">
-                    <div className="fbox-icon">
-                      <a href="#">
-                        <i className="icon-screen" />
-                      </a>
-                    </div>
-                    <div className="fbox-content">
-                      <h3>Responsive Layout</h3>
-                      <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-4">
-                  <div className="feature-box fbox-plain">
-                    <div className="fbox-icon">
-                      <a href="#">
-                        <i className="icon-eye" />
-                      </a>
-                    </div>
-                    <div className="fbox-content">
-                      <h3>Retina Ready Graphics</h3>
-                      <p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-4">
-                  <div className="feature-box fbox-plain">
-                    <div className="fbox-icon">
-                      <a href="#">
-                        <i className="icon-beaker" />
-                      </a>
-                    </div>
-                    <div className="fbox-content">
-                      <h3>Powerful Performance</h3>
-                      <p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-4">
-                  <div className="feature-box fbox-plain">
-                    <div className="fbox-icon">
-                      <a href="#">
-                        <i className="icon-ok" />
-                      </a>
-                    </div>
-                    <div className="fbox-content">
-                      <h3>12+ Header Designs</h3>
-                      <p>We have included 12+ Header + Menu Designs for your convenience so that you can match your style.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-4">
-                  <div className="feature-box fbox-plain">
-                    <div className="fbox-icon">
-                      <a href="#">
-                        <i className="icon-thumbs-up" />
-                      </a>
-                    </div>
-                    <div className="fbox-content">
-                      <h3>Awesome Mega menu</h3>
-                      <p>Completely Customizable 2 Columns, 3 Columns, 4 Columns &amp; 5 Columns Mega Menus are available with Canvas.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-4">
-                  <div className="feature-box fbox-plain">
-                    <div className="fbox-icon">
-                      <a href="#">
-                        <i className="icon-magnet" />
-                      </a>
-                    </div>
-                    <div className="fbox-content">
-                      <h3>Attractive Sticky Menu</h3>
-                      <p>Smooth &amp; Stylish Sticky Menu is what will make your Website differentiate with others.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="line" />
-            </div>
-          </div>
-          <div className="container clearfix">
-            <div className="heading-block center">
-              <h3>Available on all Major Platforms.</h3>
-              <span>We have made our App available on all Major Platforms</span>
-            </div>
-            <p className="mx-auto center" style={{ maxWidth: '800px' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo animi ab dolorem deleniti, incidunt, recusandae tenetur eius aut similique delectus nisi labore odit temporibus reprehenderit eum iure natus voluptatem commodi? Quam ea, placeat quia et dignissimos laboriosam unde earum repudiandae?</p>
-            <div className="w-100 center topmargin mb-0">
-              <a href="#" className="social-icon si-appstore si-large si-rounded si-colored inline-block" title="iOS App Store">
-                <i className="icon-appstore" />
-                <i className="icon-appstore" />
-              </a>
-              <a href="#" className="social-icon si-android si-large si-rounded si-colored inline-block" title="Android Store">
-                <i className="icon-android" />
-                <i className="icon-android" />
-              </a>
-              <a href="#" className="social-icon si-gplus si-large si-rounded si-colored inline-block" title="Windows Store">
-                <i className="icon-windows3" />
-                <i className="icon-windows3" />
-              </a>
             </div>
           </div>
         </div>
       </section>
       {/* #content end */}
+
+      <footer id="footer" className="dark" data-scrollto-settings="{&quot;offset&quot;:140,&quot;speed&quot;:1250,&quot;easing&quot;:&quot;easeOutQuad&quot;}"> {/* Copyrights ============================================= */} <div id="copyrights">
+        <div className="container-fluid">
+          <div className="row col-mb-30">
+            <div className="col-md-1"></div>
+            <div className="col-md-7 text-md-start">
+              Bản quyền thuộc về <span><a href="/">ELTEAM</a> - Hệ thống quản lý lớp học trực tuyến</span>. <br />
+              Tác giả: Trần Đăng Khoa <br />
+              <div className="copyright-links">
+                Luận văn tốt nghiệp của sinh viên trường Đại học Công Nghệ Thông Tin - Đại học Cần Thơ
+              </div>
+            </div>
+            <div className="col-md-4 text-md-start">
+              <div className="clear" />
+              <div>
+                <em class="icon ni ni-emails-fill mr-2"></em> richardktran.dev@gmail.com
+              </div>
+              <div>
+                <em class="icon ni ni-call-alt-fill mr-2"></em> (+84) 947 685 343
+              </div>
+              <div>
+                <em class="icon ni ni-cards-fill mr-2"></em>
+                B1805879
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>{/* #copyrights end */} </footer>
     </div>
   );
 }
